d373bd1e959c649aba52b033f1e14934e85fa28d
siwakorn.k <siwakorn.k@blendata.co>
2024-07-13 01:27:44 +0700

    first golang with mongodblib insert

--- main.go
+++ main.go
@@ -3,51 +3,67 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"net/http"
 	"time"
 )
 
+var client *mongo.Client
+
 func main() {
-	// URL ของ MongoDB Atlas
-	clientOptions := options.Client().ApplyURI("apikey")
+	// ตั้งค่า Gin
+	router := gin.Default()
 
-	// สร้าง client และเชื่อมต่อไปยัง MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// ตรวจสอบการเชื่อมต่อ
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to MongoDB!")
-
-	// เลือกฐานข้อมูลและคอลเลคชัน
-	collection := client.Database("test").Collection("numbers")
-
-	// สร้าง context สำหรับการทำงานกับ MongoDB
+	// ตั้งค่า MongoDB
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	// ตัวอย่างการเพิ่มข้อมูลลงในคอลเลคชัน
-	doc := bson.D{{"name", "pi"}, {"value", 3.14159}}
-	result, err := collection.InsertOne(ctx, doc)
+	clientOptions := options.Client().ApplyURI("mongodb+srv://siwakornqwer:m0WfmokyC2YAp3lt@cluster0.qsog5xm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
-
-	// ปิดการเชื่อมต่อ
-	if err := client.Disconnect(ctx); err != nil {
+	err = client.Ping(ctx, nil)
+	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Connected to MongoDB!")
 
-	fmt.Println("Connection to MongoDB closed.")
+	// สร้างเส้นทาง API
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/users", getUsers)
+
+	// เริ่มต้นเซิร์ฟเวอร์
+	router.Run(":8080")
+}
+
+func getUsers(c *gin.Context) {
+	collection := client.Database("test").Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer cursor.Close(ctx)
+
+	var users []bson.M
+	if err = cursor.All(ctx, &users); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, users)
 }
